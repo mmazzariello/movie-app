6c856cf6120c9cdb5642e76ee3e31b11d43d2fb5
Mariana Mazzariello <mfmazzariello@gmail.com>
2021-11-21 20:07:08 +0100

    ADD html, css and fetch req to movies API

--- script.js
+++ script.js
@@ -1,60 +1,36 @@
-const smallCups = document.querySelectorAll(".cup-small");
-const liters = document.getElementById("liters");
-const percentage = document.getElementById("percentage");
-const remained = document.getElementById("remained");
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=dca780967c9ba226d8dba710b92dadde&page=1";
 
-console.log(percentage, "hola");
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_API =
+  "https://api.themoviedb.org/3/search/movie?api_key=dca780967c9ba226d8dba710b92dadde&query=''";
 
-updateBigCup();
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
-smallCups.forEach(function (cup, idx) {
-  // console.log(cup, idx);
+//Get initial movies
 
-  cup.addEventListener("click", () => highlight(idx));
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}
+
+//Search
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_API + searchTerm);
+
+    search.value = "";
+  } else {
+    window.location.reload;
+  }
 });
-
-function highlight(idx) {
-  if (
-    smallCups[idx].classList.contains("full") &&
-    !smallCups[idx].nextElementSibling.classList.contains("full")
-  ) {
-    idx--;
-  }
-
-  smallCups.forEach((cup, idx2) => {
-    if (idx2 <= idx) {
-      cup.classList.add("full");
-    } else {
-      cup.classList.remove("full");
-    }
-  });
-
-  updateBigCup();
-}
-
-function updateBigCup() {
-  const fullCups = document.querySelectorAll(".cup-small.full").length;
-  console.log(fullCups);
-
-  const totalCups = smallCups.length;
-
-  console.log(totalCups);
-
-  if (fullCups === 0) {
-    console.log(percentage);
-    percentage.style.visibility = "hidden";
-    percentage.style.height = 0;
-  } else {
-    percentage.style.visibility = "visible";
-    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
-    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
-  }
-
-  if (fullCups === totalCups) {
-    remained.style.visibility = "hidden";
-    remained.style.height = 0;
-  } else {
-    remained.style.visibility = "visible";
-    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
-  }
-}
